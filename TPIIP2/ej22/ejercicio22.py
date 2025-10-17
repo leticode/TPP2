0a3986abe579441f398781b8b059a77b2f369357
Leticia <leticiasoulos@gmail.com>
2025-10-16 21:36:12 -0300

    ejercicio 22 casi terminado

--- TPIIP2/ej22/ejercicio22.py
+++ TPIIP2/ej22/ejercicio22.py
@@ -3,13 +3,25 @@ vehículo en la flota transporta un envío. Un envío puede estar compuesto por 
 (uno o más productos) cuya vida depende totalmente de dicho envío. Si un paquete no se
 entrega, el paquete se “destruye” pero no así los productos.'''
 
-        
+class Producto:
+    def __init__(self, nombre, precio):
+        self.nom = nombre
+        self.precio = precio  
+    def __str__(self, nombre, precio):
+        return self.nom + self.precio
+class Paquete:
+    def __init__(self, productos):
+        self.productos = productos
+
+    def __str__(self):
+        return f"Paquete con productos: {[p.nombre for p in self.productos]}" 
+                
 class Vehiculo:
-    def __init__(self, patente, modelo, marca, conductor):
+    def __init__(self, patente, envio):
         self.patente = patente
-        self.modelo = modelo
-        self.marca = marca  
-        self.conductor = conductor 
+        self.envio = envio
+    def __str__(self):
+        return    
 class Camiones(Vehiculo):   
     def __init__(self, patente, modelo, marca, conductor):
         super().__init__(patente, modelo, marca, conductor) 
